b619e4704a367e1046cd435093c98f91c9267e5d
pawan779 <pawan.dharel77@gmail.com>
2019-12-10 09:56:42 +0545

    button design created

---
+++ app/src/main/java/com/fragment/Area.java
@@ -0,0 +1,36 @@
+package com.fragment;
+
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.pawan.secondasignment.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class Area extends Fragment implements View.OnClickListener{
+    private Button btnArea;
+    private EditText etRadius;
+
+
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+    }
+
+    @Override
+    public void onClick(View v) {
+
+    }
+}
