459e1b0cff899cae10a172be8c3f483e6d962780
pawan779 <pawan.dharel77@gmail.com>
2019-12-10 10:19:04 +0545

    area of circle added

--- app/src/main/java/com/fragment/Area.java
+++ app/src/main/java/com/fragment/Area.java
@@ -10,6 +10,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 import com.pawan.secondasignment.R;
 
@@ -19,6 +20,7 @@ import com.pawan.secondasignment.R;
 public class Area extends Fragment implements View.OnClickListener{
     private Button btnArea;
     private EditText etRadius;
+    private TextView tvArea;
 
 
 
@@ -27,10 +29,21 @@ public class Area extends Fragment implements View.OnClickListener{
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
+        View view=inflater.inflate(R.layout.fragment_area,container,false);
+        etRadius=view.findViewById(R.id.etRadius);
+        btnArea=view.findViewById(R.id.btnArea);
+        tvArea=view.findViewById(R.id.tvArea);
+        btnArea.setOnClickListener(this);
+        return view;
     }
 
     @Override
     public void onClick(View v) {
+        float radius=Float.parseFloat(etRadius.getText().toString());
+        float area=3.14f*radius*radius;
+
+        tvArea.setText("Area of circle is "+area);
+
 
     }
 }
--- app/src/main/java/com/pawan/secondasignment/MainActivity.java
+++ app/src/main/java/com/pawan/secondasignment/MainActivity.java
@@ -1,14 +1,40 @@
 package com.pawan.secondasignment;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+import com.fragment.Area;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+private Button btnArea;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btnArea=findViewById(R.id.btnArea);
+        btnArea.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        FragmentManager fragmentManager=getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction=fragmentManager.beginTransaction();
+        switch (v.getId()){
+            case R.id.btnArea:
+                Area area=new Area();
+                fragmentTransaction.replace(R.id.fragmentContainer,area);
+                fragmentTransaction.commit();
+                break;
+
+        }
+
     }
 }
